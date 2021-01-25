8b74b905e8e05a6a3c8a7ef4524029f1307e61c4
joshuadl12 <47647612+joshuadl12@users.noreply.github.com>
2021-01-24 19:40:37 -0600

    Update all.js

--- src/methods/all.js
+++ src/methods/all.js
@@ -1,9 +1,9 @@
 module.exports = function(db, params, options) {
   
   // Fetch Entry
-  var stmt = db.prepare(`SELECT * FROM ${options.table} WHERE ID IS NOT NULL`);
+  var stmt = db.query(`SELECT * FROM ${options.table} WHERE ID IS NOT NULL`);
   let resp = [];
-  for (var row of stmt.iterate()) {
+  for (var row of stmt) {
     try {
       resp.push({
         ID: row.ID,
