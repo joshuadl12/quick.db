66b15eb7845ad39a92937c9147515c2e2ce503ef
joshuadl12 <47647612+joshuadl12@users.noreply.github.com>
2021-01-25 07:45:23 -0600

    Update delete.js

--- src/methods/delete.js
+++ src/methods/delete.js
@@ -4,7 +4,7 @@ const unset = require('lodash/unset');
 module.exports = function(db, params, options) {
   
   // Fetch Entry
-  let fetched = db.prepare(`SELECT * FROM ${options.table} WHERE ID = (?)`).get(params.id);
+  let fetched = db.query(`SELECT * FROM ${options.table} WHERE ID = ${params.id}`)
   if (!fetched) return false; // If empty, return null
   else fetched = JSON.parse(fetched.json);
   try { fetched = JSON.parse(fetched) } catch (e) {}
@@ -13,11 +13,11 @@ module.exports = function(db, params, options) {
   if (typeof fetched === 'object' && params.ops.target) {
     unset(fetched, params.ops.target);
     fetched = JSON.stringify(fetched);
-    db.prepare(`UPDATE ${options.table} SET json = (?) WHERE ID = (?)`).run(fetched, params.id);
+    db.query(`UPDATE ${options.table} SET json = ${fetched} WHERE ID = ${params.id}`);
     return true;
   }
   else if (params.ops.target) throw new TypeError('Target is not an object.');
-  else db.prepare(`DELETE FROM ${options.table} WHERE ID = (?)`).run(params.id);
+  else db.query(`DELETE FROM ${options.table} WHERE ID = ${params.id}`);
   
   // Resolve
   return true;
